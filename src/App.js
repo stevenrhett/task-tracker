9c2562caeb49aec25430bcc9ea104843422239ec
Snisken <Mr.Snisken1@gmail.com>
2022-10-01 19:46:14 -0400

    Added about me page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SignUpPage from "./SignUpPage";
 import LogInForm from "./LogInForm";
+import AboutUsPageB from "./AboutUsPageB";
 
 function App() {
   return(
   <div>
+      <AboutUsPageB/>
       <LogInForm/>
       <SignUpPage/>
   </div>
