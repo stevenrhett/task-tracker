5d3ece805c1eee85cc0489df3399bb5fceee3865
Didier Maniraho <maniraho.didier2190@gmail.com>
2022-10-03 19:50:52 -0400

    innitial Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import HeaderComponent from "./HeaderComponent";
 
 function App() {
   return(
   <div>
-  <h1 className="text-3xl bg-pink-500">HELLO HELLO</h1>
-
+  <HeaderComponent/>
   </div>
   );
 }
