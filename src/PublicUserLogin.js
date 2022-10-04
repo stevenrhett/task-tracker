28050e0d518b79979c996404901c7f86db241852
stevenrhett <johnsonsteven18@gmail.com>
2022-10-04 15:39:28 -0400

    adding styling to the two seperate public login components

--- src/PublicUserLogin.js
+++ src/PublicUserLogin.js
@@ -10,39 +10,38 @@ const PublicUserLogin = () => {
                     <tbody className="text-teal-600">
 
                     <tr >
-                        <th>1</th>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox bg-orange-400"/>
+                            </label>
+                        </th>
                         <td>12/05/2022</td>
                         <td>Learn what ARP means?</td>
                         <td>Computer Science</td>
                     </tr>
 
                     <tr>
-                        <th>2</th>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox bg-orange-400"/>
+                            </label>
+                        </th>
                         <td>12/07/2022</td>
                         <td>Read Moby Dick</td>
                         <td>English Lit</td>
                     </tr>
 
                     <tr>
-                        <th>3</th>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox bg-orange-400"/>
+                            </label>
+                        </th>
                         <td>12/09/2022</td>
                         <td>Learn times tables</td>
                         <td>Math</td>
                     </tr>
 
-                    <tr>
-                        <th>4</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-
-                    <tr>
-                        <th>5</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
                     </tbody>
                 </table>
             </div>
