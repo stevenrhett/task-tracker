2b4aedaca55ce6d1049b66080c5ee9474134cbfe
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 12:57:58 -0400

    creating public user login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './index.css';
 import PublicLoginPage from "./PublicLoginPage";
+import PublicUserLogin from "./PublicUserLogin";
 
 function App() {
     return (
         <div>
             <PublicLoginPage/>
-
+            <PublicUserLogin/>
         </div>
     );
 }
