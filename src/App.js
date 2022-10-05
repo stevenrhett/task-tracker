a29390c5935512c6f727cf498bbf9817c696376c
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 15:25:43 -0400

    testing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderComponent from "./HeaderComponent";
+import ModalComponent from "./ModalComponent";
 
 function App() {
   return(
   <div>
   <HeaderComponent/>
+    <ModalComponent/>
   </div>
   );
 }
