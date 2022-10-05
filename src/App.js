adb5c3d36d08838016cf6d13b3c9b7e1226730b6
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 12:54:38 -0400

    seperating the components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './index.css';
 import PublicLoginPage from "./PublicLoginPage";
-import PublicUserLogin from "./PublicUserLogin";
 
 function App() {
     return (
         <div>
             <PublicLoginPage/>
-            <PublicUserLogin/>
+
         </div>
     );
 }
