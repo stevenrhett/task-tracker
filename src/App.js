08b00b561b750a6f0a72f932dae058598e867920
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 16:55:36 -0400

    fixing code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import './index.css';
 import PublicLoginPage from "./PublicLoginPage";
 import HeaderComponent from "./HeaderComponent";
@@ -19,52 +17,4 @@ function App() {
     );
 }
 
-import React from 'react';
-
-const PublicUserLogin = () => {
-    return (
-        <div>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    <tbody className="text-teal-600">
-                    <tr >
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox bg-orange-400"/>
-                            </label>
-                        </th>
-                        <td>12/05/2022</td>
-                        <td>Learn what ARP means?</td>
-                        <td>Computer Science</td>
-                    </tr>
-
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox bg-orange-400"/>
-                            </label>
-                        </th>
-                        <td>12/07/2022</td>
-                        <td>Read Moby Dick</td>
-                        <td>English Lit</td>
-                    </tr>
-
-                    <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox bg-orange-400"/>
-                            </label>
-                        </th>
-                        <td>12/09/2022</td>
-                        <td>Learn times tables</td>
-                        <td>Math</td>
-                    </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
-};
-
-export default PublicUserLogin;
+export default App;
