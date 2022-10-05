8990304a1f7684960f42ef4025df47e2dffb190f
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 16:02:02 -0400

    changing the case-sensitive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './index.css';
 import PublicLoginPage from "./PublicLoginPage";
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
+import LogInForm from "./LogInForm";
 
 function App() {
     return (
         <div>
 
             <HeaderComponent/>
+            <LogInForm/>
             <PublicLoginPage/>
             <FooterComponent/>
 
