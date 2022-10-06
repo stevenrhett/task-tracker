9e93fbd3e433b738644ba77c526ac5d160f239aa
stevenrhett <johnsonsteven18@gmail.com>
2022-10-05 20:01:33 -0400

    fixing meet the team page

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
-
-import SignUpPage from "./SignUpPage";
-import LogInForm from "./LogInForm";
-
-import TaskCardB from "./TaskCardB";
-import DisplayAllTasks from "./DisplayAllTasks";
-
-import HeaderComponent from "./HeaderComponent";
-import FooterComponent from "./FooterComponent";
-
-
-
-
-
-function App() {
-  return(
-  <div>
-
-
-
-
-
-      <HeaderComponent/>
-
 import './index.css';
 import PublicLoginPage from "./PublicLoginPage";
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
-import LogInForm from "./LogInForm";
 
 function App() {
     return (
         <div>
-
-
             <HeaderComponent/>
             <PublicLoginPage/>
             <FooterComponent/>
 
 
-      <SignUpPage/>
-<FooterComponent/>
-
-  </div>
-  );
-
         </div>
     );
-
 }
+
+
 export default App;
