91e2f5aa17b31e4b6e816b2fe22984441b15f4ca
stevenrhett <johnsonsteven18@gmail.com>
2022-10-06 09:09:07 -0400

    corrections

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import HeaderComponent from "./HeaderComponent";
 import LogInForm from "./LogInForm";
 import FooterComponent from "./FooterComponent";
 import MeetTheTeam from "./MeetTheTeam";
+import WelcomeComponent from "./WelcomeComponent";
 
 function App() {
     return (
         <div>
             <HeaderComponent/>
-            <MeetTheTeam/>
+           <WelcomeComponent/>
             <FooterComponent/>
         </div>
 
--- src/MeetTheTeam.js
+++ src/MeetTheTeam.js
@@ -1,42 +1,204 @@
 import React from 'react';
+import {Icon} from '@iconify/react';
 
 const MeetTheTeam = () => {
     return (<div>
-        <div className="divider lg:divider-horizontal font-bold">Didier</div>
-        <div className="flex flex-col w-full lg:flex-row">
-            <div className="grid flex-grow h-32 card bg-orange-400 rounded-box place-items-center">
-                <div className="avatar">
-                    <div className="w-24 rounded mx-4">
-                        <img src="https://placeimg.com/192/192/people"/>
-                    </div>
-                </div>
-            </div>
-            {/*...*/}
-            <div className="divider lg:divider-horizontal font-bold">Steve</div>
-            <div className="grid flex-grow h-32 card bg-teal-500 rounded-box place-items-center">
-                <div className="avatar">
-                    <div className="w-24 rounded mx-4">
-                        <img src="https://placeimg.com/192/192/people"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {/*...*/}
-        <div className="divider lg:divider-horizontal font-bold">Davola</div>
-        <div className="flex flex-col w-full lg:flex-row">
-            <div className="grid flex-grow h-32 card bg-orange-400 rounded-box place-items-center">
-                <div className="avatar">
-                    <div className="w-24 rounded mx-4">
-                        <img src="https://placeimg.com/192/192/people"/>
-                    </div>
-                </div>
-            </div>
-            {/*...*/}
-            <div className="divider lg:divider-horizontal font-bold">Justin</div>
-            <div className="grid flex-grow h-32 card bg-teal-500 rounded-box place-items-center">
-                <div className="avatar">
-                    <div className="w-24 rounded mx-4">
-                        <img src="https://placeimg.com/192/192/people"/>
+        <div className="flex items-center justify-center min-h-screen bg-white py-48">
+            <div className="flex flex-col">
+                <div className="flex flex-col mt-2">
+
+                    <div className="container max-w-7xl px-4">
+
+                        <div className="flex flex-wrap justify-center text-center mb-12">
+                            <div className="w-full lg:w-6/12 px-4">
+
+                                <h1 className="text-teal-500 text-4xl font-bold mb-8">
+                                    MSIMBO: ULEM Coding Bootcamp
+                                </h1>
+
+                                <p className="text-gray-700 text-lg font-light">
+                                    MSIMBO bootcamp trains talented,
+                                    low-income individuals to become full-stack web developers and launch a new
+                                    career. Students are trained
+                                    in web
+                                    development, effective business communication, and leadership. We will conduct a
+                                    20 weeks of 120 hours
+                                    coding technology
+                                    training program and the professional development for ULEM’s MSIMBO program.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-wrap">
+
+                            <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                                <div className="flex flex-col">
+
+                                    <a href="#" className="mx-auto">
+                                        <img
+                                            className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                            src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=70"
+                                            alt="alt"/>
+                                    </a>
+
+                                    <div className="text-center mt-6">
+
+                                        <h1 className="text-teal-600 text-xl font-bold mb-1">
+                                            Didier
+                                        </h1>
+
+                                        <div className="text-orange-400 font-light mb-2">
+                                            Full-time Student
+                                        </div>
+
+                                        <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                            <a href="https://www.linkedin.com"
+                                               className="flex rounded-full h-10 w-10 bg-base-100">
+                                                <Icon icon="mdi:linkedin" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.twitter.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi-twitter" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.instagram.com"
+                                               className="flex rounded-full  h-10 w-10">
+                                                <Icon icon="mdi-instagram" width="26" height="26"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                                <div className="flex flex-col">
+
+                                    <a href="#" className="mx-auto">
+                                        <img
+                                            className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                            src="https://images.unsplash.com/photo-1634896941598-b6b500a502a7?fit=clamp&w=400&h=400&q=80"
+                                            alt="alt"/>
+                                    </a>
+
+                                    <div className="text-center mt-6">
+
+                                        <h1 className="text-teal-600 text-xl font-bold mb-1">
+                                            Davola
+                                        </h1>
+
+                                        <div className="text-orange-400 font-light mb-2">
+                                            Student
+                                        </div>
+
+                                        <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                            <a href="https://www.linkedin.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi:linkedin" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.twitter.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi-twitter" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.instagram.com"
+                                               className="flex rounded-full  h-10 w-10">
+                                                <Icon icon="mdi-instagram" width="26" height="26"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                                <div className="flex flex-col">
+
+                                    <a href="#" className="mx-auto">
+                                        <img
+                                            className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                            src="https://images.unsplash.com/photo-1634193295627-1cdddf751ebf?fit=clamp&w=400&h=400&q=80"
+                                            alt="alt"/>
+                                    </a>
+
+                                    <div className="text-center mt-6">
+
+                                        <h1 className="text-teal-600 text-xl font-bold mb-1">
+                                            Justin
+                                        </h1>
+
+                                        <div className="text-orange-400 font-light mb-2">
+                                            Team Member
+                                        </div>
+
+                                        <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                            <a href="https://www.linkedin.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi:linkedin" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.twitter.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi-twitter" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.instagram.com"
+                                               className="flex rounded-full  h-10 w-10">
+                                                <Icon icon="mdi-instagram" width="26" height="26"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                                <div className="flex flex-col">
+
+                                    <a href="#" className="mx-auto">
+                                        <img
+                                            className="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                            src="https://images.unsplash.com/photo-1635003913011-95971abba560?fit=clamp&w=400&h=400&q=80"
+                                            alt="alt"/>
+                                    </a>
+
+                                    <div className="text-center mt-6">
+
+                                        <h1 className="text-teal-600 text-xl font-bold mb-1">
+                                            Kim
+                                        </h1>
+
+                                        <div className="text-orange-400 font-light mb-2">
+                                            Student
+                                        </div>
+
+                                        <div className="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+
+                                            <a href="https://www.linkedin.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi:linkedin" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.twitter.com"
+                                               className="flex rounded-full h-10 w-10">
+                                                <Icon icon="mdi-twitter" width="26" height="26"/>
+                                            </a>
+
+                                            <a href="https://www.instagram.com"
+                                               className="flex rounded-full  h-10 w-10">
+                                                <Icon icon="mdi-instagram" width="26" height="26"/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,5 +209,3 @@ const MeetTheTeam = () => {
 };
 
 export default MeetTheTeam;
-
-
